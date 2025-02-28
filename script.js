1e8859315581bf54f53e3a89b25fd4cc6b5bc5b9
Jeremias Escudero <jeremiasescudero67@gmail.com>
2025-02-28 10:16:19 -0300

    defer atribute added to Script

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
 function loginWithAzure() {
     window.location.href = "https://gambit3teee3.b2clogin.com/gambit3teee3.onmicrosoft.com/oauth2/v2.0/authorize?" +
-        "p=B2C_1_SignUpSignIn" +  // Replace with your actual User Flow name
-        "&client_id=9a2b7851-9c08-4fb1-a88b-7d3c48f47e95" +  // Your Azure AD B2C Client ID
+        "p=B2C_1_SIGNUP_LOGIN" +  
+        "&client_id=9a2b7851-9c08-4fb1-a88b-7d3c48f47e95" +  
         "&nonce=defaultNonce" +
-        "&redirect_uri=https%3A%2F%2Fgambit3.teee3.com%2Fsignin-oidc" +  // Redirect URI (must match your Azure settings)
+        "&redirect_uri=https://gambit3.teee3.com/signin-oidc" +  
         "&scope=openid%20profile%20email" +
         "&response_type=id_token" +
         "&prompt=login";
